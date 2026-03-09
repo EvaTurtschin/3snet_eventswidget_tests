9e9d85c96acfc1b65c8d4437985759ec6ea64ef0
AIT TR Student <evgeniya@turtschin@gmail.com>
2026-03-09 12:55:54 +0100

    NEW: test.step added as an example to test TC-01.ts

--- tests/TC-01_Generation_with_default_data.spec.ts
+++ tests/TC-01_Generation_with_default_data.spec.ts
@@ -3,30 +3,66 @@ import BasePage from '@pages/BasePage';
 
 
 test('TC-01 Генерация кода iframe и превью с дефолтными значениями', async ({ page }) => {
-  const basePage = new BasePage(page);
-  await page.goto('');
+    const basePage = new BasePage(page);
+    const generateBtn = page.getByRole('button', { name: 'Сгенерировать превью' });
+    let iframeCode: string;
+
+  await test.step('Открываем страницу https://dev.3snet.info/eventswidget/', async () => {
+    await page.goto('');
+  });
   
   // Не изменяя настройки, нажимаем на кнопку "Сгенерировать превью"
-  const generateBtn = page.getByRole('button', { name: 'Сгенерировать превью' });
-  await basePage.generatePreviewWithRetry(generateBtn);
+  await test.step('Не изменяя настройки, нажимаем на кнопку "Сгенерировать превью"', async () => {
+    await basePage.generatePreviewWithRetry(generateBtn);
+  });
 
-  await basePage.checkCodeTextareaVisible();
+  await test.step('Ожидаем генерацию кода в iframe', async () => {
+    await basePage.checkCodeTextareaVisible();
+  });
 
-  // Получаем данные кода iframe
-  const iframeCode = await basePage.getIframeCode();
-  // Проверяем данные кода iframe на сответствие дефолтным значениям
+  await test.step('Получаем код iframe', async () => {
+  iframeCode = await basePage.getIframeCode();
+  });
+
+ // Проверяем данные кода iframe на сответствие дефолтным значениям
+  await test.step('Проверяем значение цветовой темы по дефолту в коде iframe', async () => {
   expect(iframeCode).toContain('theme=turquoise');
+  });
+  await test.step('Проверяем значение ширины по дефолту в коде iframe', async () => {
   expect(iframeCode).toContain('width="230"');
+  });
+  await test.step('Проверяем значение высоты по дефолту в коде iframe', async () => {
   expect(iframeCode).toContain('height="240"');
-  expect(iframeCode).toContain('frameborder="0"');
+  });
+  await test.step('Проверяем значение темы по дефолту в коде iframe', async () => {
+  expect(iframeCode).toContain('event_type=');
+  });
+  await test.step('Проверяем значение страны по дефолту в коде iframe', async () => {
+  expect(iframeCode).toContain('event_country=');
+  });
+
 
   // Ждем генерации превью
+  await test.step('Ждем загррузки превью', async () => {
   await basePage.checkPreviewVisible();
   await basePage.waitForIframeAttached();
+  });
+
   // Проверяем данные превью
-  await expect(basePage.iframePreview).toHaveAttribute('src', /theme=turquoise/);
-  await expect(basePage.iframePreview).toHaveAttribute('width', '230');
-  await expect(basePage.iframePreview).toHaveAttribute('height', '240');
-  await expect(basePage.iframePreview).toHaveAttribute('frameborder', '0');
+  await test.step('Проверяем значение цветовой темы по дефолту в превью', async () => {
+    await expect(basePage.iframePreview).toHaveAttribute('src', /theme=turquoise/);
+  });
+  await test.step('Проверяем значение ширины по дефолту в превью', async () => {
+     await expect(basePage.iframePreview).toHaveAttribute('width', '230');
+  });
+  await test.step('Проверяем значение высоты по дефолту в превью', async () => {
+     await expect(basePage.iframePreview).toHaveAttribute('height', '240');
+  });
+  await test.step('Проверяем значение темы по дефолту в превью', async () => {
+     await expect(basePage.iframePreview).toHaveAttribute('src', /event_type=/);
+  });
+  await test.step('Проверяем значение страны по дефолту в превью', async () => {
+     await expect(basePage.iframePreview).toHaveAttribute('src', /event_country=/);
+  });
 
 });
